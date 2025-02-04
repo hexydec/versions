0982b243f45ca73e792df60a5cb7b4efb5399fc7
Hexydec <will@hexydec.com>
2025-02-04 18:12:09 +0000

    Reworked `generate` as `browsers`. Added `browsers::build()` to control building the versions and report back. Added `browsers::msg()` to enable a reporting callback, it also prints to the screen by default. Output will now only be generated in one file. Added a `rebuild` option which regenerates the whole file, otherwise it just updates the existing file, this also doesn't fetch legacy browsers and only fetches the first page of results (If applicable). Added `konqueror`, `ucbrowser`, `silk`, `waterfox`, and `palemoon` generators.

--- src/autoload.php
+++ src/autoload.php
@@ -5,7 +5,7 @@ require \dirname(__DIR__).'/vendor/autoload.php';
 
 \spl_autoload_register(function (string $class) : void {
 	$classes = [
-		'hexydec\\versions\\generate' => __DIR__.'/generate.php'
+		'hexydec\\versions\\browsers' => __DIR__.'/browsers.php'
 	];
 	if (isset($classes[$class])) {
 		require $classes[$class];
--- src/browsers.php
+++ src/browsers.php
@@ -1,12 +1,96 @@
 <?php
 declare(strict_types = 1);
 namespace hexydec\versions;
+class browsers {
 
-class generate {
-
+	protected array $config = [
+		'msg' => null, // a callback for where the messages are written
+		'cache' => null // to cache the source content, specify the absolute cache directory
+	];
 	protected array $timing = [];
 
-	public function fetch(string $url, ?string $cache = null, bool $contents = true) : string|false {
+	public function __construct(array $config = []) {
+		$this->config = $config;
+	}
+
+	public function build(string $target, bool $rebuild = false) : bool {
+		$browsers = [
+			'chrome' => [$this, 'getChromeVersions'],
+			'firefox' => [$this, 'getFirefoxVersions'],
+			'edge' => [$this, 'getEdgeVersions'],
+			'safari' => [$this, 'getSafariVersions'],
+			'ie' => [$this, 'getInternetExplorerVersions'],
+			'opera' => [$this, 'getOperaVersions'],
+			'brave' => [$this, 'getBraveVersions'],
+			'vivaldi' => [$this, 'getVivaldiVersions'],
+			'maxthon' => [$this, 'getMaxthonVersions'],
+			'samsung' => [$this, 'getSamsungInternetVersions'],
+			'huawei' => [$this, 'getHuaweiBrowserVersions'],
+			'kmeleon' => [$this, 'getKmeleonVersions'],
+			'konqueror' => [$this, 'getKonquerorVersions'],
+			'ucbrowser' => [$this, 'getUcBrowserVersions'],
+			'silk' => [$this, 'getSilkBrowserVersions'],
+			'waterfox' => [$this, 'getWaterfoxVersions'],
+			'palemoon' => [$this, 'getPaleMoonVersions']
+		];
+
+		// read existing file
+		if ($rebuild || !\file_exists($target)) {
+			$data = [];
+		} elseif (($json = \file_get_contents($target)) === false) {
+			\trigger_error('Could not read file', E_USER_WARNING);
+			$data = [];
+		} elseif (($data = \json_decode($json, true)) === false) {
+			\trigger_error('Data is not valid JSON', E_USER_WARNING);
+			$data = [];
+		}
+
+		// update browser versions
+		$added = 0;
+		$total = 0;
+		foreach ($browsers AS $key => $item) {
+			if (($results = \call_user_func($item, $rebuild)) !== false) {
+				$new = $rebuild ? $results : \array_diff_key($results, $data[$key]);
+				$count = \count($new);
+				$this->msg('Found '.$count.' for '.\ucfirst($key));
+				$data[$key] = $rebuild ? $results : \array_replace($data[$key], $results);
+				\arsort($data[$key]);
+				$added += $count;
+				$total += \count($data[$key]);
+			} else {
+				\trigger_error('Could not generate versions for '.\ucfirst($key), E_USER_WARNING);
+			}
+		}
+
+		// generate output file
+		$dir = \dirname($target);
+
+		// no browsers found
+		if ($total === 0) {
+			\trigger_error('No browser versions could be generated', E_USER_WARNING);
+
+		// create output directory
+		} elseif (!\is_dir($dir) && !\mkdir($dir, 0755)) {
+			\trigger_error('Output directory could not be created', E_USER_WARNING);
+
+		// write output file
+		} elseif (\file_put_contents($target, \json_encode($data, JSON_PRETTY_PRINT)) === false) {
+			\trigger_error('Could not save output file', E_USER_WARNING);
+
+		// complete
+		} else {
+			$this->msg($rebuild ? 'Saved '.$total.' browser versions' : 'Added '.$added.' and saved '.$total.' browser versions');
+			return true;
+		}
+		return false;
+	}
+
+	protected function msg(string $msg) : void {
+		\call_user_func($this->config['msg'] ?? function (string $msg) : void {echo $msg."\n";}, $msg);
+	}
+
+	protected function fetch(string $url, bool $contents = true) : string|false {
+		$cache = $this->config['cache'];
 		$local = null;
 
 		// generate cache file name
@@ -35,7 +119,7 @@ class generate {
 			}
 			$context = \stream_context_create([
 				'http' => [
-					'user_agent' => 'Mozilla/5.0 (compatible; Hexydec IP Ranges Bot/1.0; +https://github.com/hexydec/ip-ranges/)',
+					'user_agent' => 'Mozilla/5.0 (compatible; Hexydec Browser Versions Bot/1.0; +https://github.com/hexydec/versions/)',
 					'header' => [
 						'Sec-Fetch-Dest: document',
 						'Sec-Fetch-Mode: navigate',
@@ -49,7 +133,6 @@ class generate {
 			]);
 			if ($contents) {
 				if (($file = \file_get_contents($url, false, $context)) !== false) {
-					\sleep(3);
 				
 					// save to local file
 					if ($local !== null) {
@@ -69,8 +152,8 @@ class generate {
 		return false;
 	}
 
-	public function getFromJson(string $url, array $version, array $date, string $cache = null) {
-		if (($file = $this->fetch($url, $cache)) !== false && ($json = \json_decode($file)) !== null && !empty($json)) {
+	protected function getFromJson(string $url, array $version, array $date) {
+		if (($file = $this->fetch($url)) !== false && ($json = \json_decode($file)) !== null && !empty($json)) {
 			$data = [];
 			foreach ($json AS $item) {
 				$row = [
@@ -93,29 +176,26 @@ class generate {
 		return false;
 	}
 
-	public function getChromeVersions(?string $cache = null) : array|false {
-		$items = 1000;
+	protected function getChromeVersions(bool $rebuild = false) : array|false {
+		$items = $rebuild ? 1000 : 20;
 		$url = 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows&num='.$items.'&offset=';
 		$chrome = [];
 		for ($i = 0; $i < 10; $i++) {
-			if (($data = $this->getFromJson($url.($i * $items), ['version'], ['time'], $cache)) !== false) {
+			if (($data = $this->getFromJson($url.($i * $items), ['version'], ['time'])) !== false) {
 				$chrome = \array_merge($chrome, \array_map(fn (int $time) => \date('Y-m-d', \intval($time / 1000)), $data));
-				if (\count($data) < $items) {
+				if (\count($data) < $items || !$rebuild) {
 					break;
-				} else {
-					sleep(5);
 				}
 			} else {
 				break;
 			}
 		}
-		\ksort($chrome, SORT_NUMERIC);
 		return $chrome ?: false;
 	}
 
-	public function getFirefoxVersions(?string $cache = null) : array|false {
+	protected function getFirefoxVersions(bool $rebuild = false) : array|false {
 		$url = 'https://whattrainisitnow.com/calendar/';
-		if (($html = $this->fetch($url, $cache)) !== false) {
+		if (($html = $this->fetch($url)) !== false) {
 			$data = [];
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
@@ -127,16 +207,15 @@ class generate {
 						}
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
 				return $data;
 			}
 		}
 		return false;
 	}
 
-	protected function getLegacyEdgeVersions(?string $cache = null) : array|false {
+	protected function getLegacyEdgeVersions() : array|false {
 		$url = 'https://en.wikipedia.org/wiki/EdgeHTML';
-		if (($html = $this->fetch($url, $cache)) !== false) {
+		if (($html = $this->fetch($url)) !== false) {
 			$data = [];
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
@@ -147,17 +226,16 @@ class generate {
 						$data[\trim($cells->eq(0)->text())] = $date->format('Y-m-d');
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
 				return $data;
 			}
 		}
 		return false;
 	}
 
-	public function getEdgeVersions(?string $cache = null) : array|false {
+	protected function getEdgeVersions(bool $rebuild = false) : array|false {
 		$url = 'https://learn.microsoft.com/en-us/deployedge/microsoft-edge-release-schedule';
-		if (($html = $this->fetch($url, $cache)) !== false) {
-			$data = $this->getLegacyEdgeVersions($cache);
+		if (($html = $this->fetch($url)) !== false) {
+			$data = $rebuild ? $this->getLegacyEdgeVersions() : [];
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
 				foreach ($obj->find('table > tbody > tr') AS $row) {
@@ -166,7 +244,6 @@ class generate {
 						$data[$match[2]] = $date->format('Y-m-d');
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
 				return $data;
 			}
 		}
@@ -205,10 +282,10 @@ class generate {
 		];
 	}
 
-	public function getSafariVersions(?string $cache = null) : array|false {
+	protected function getSafariVersions(bool $rebuild = false) : array|false {
 		$url = 'https://developer.apple.com/tutorials/data/documentation/safari-release-notes.json';
-		if (($file = $this->fetch($url, $cache)) !== false && ($json = \json_decode($file)) !== null) {
-			$data = $this->getLegacySafariVersions();
+		if (($file = $this->fetch($url)) !== false && ($json = \json_decode($file)) !== null) {
+			$data = $rebuild ? $this->getLegacySafariVersions() : [];
 			foreach ($json->references AS $item) {
 				$text = $item->abstract[0]->text;
 				if (\preg_match('/([a-z]++ [0-9]{1,2}, [0-9]{4})[^0-9]++([0-9.]++) \(([0-9.]++)\)/i', $text, $match)) {
@@ -216,13 +293,12 @@ class generate {
 					$data[$match[2]] = $date->format('Y-m-d');
 				}
 			}
-			\ksort($data, SORT_NUMERIC);
 			return $data;
 		}
 		return false;
 	}
 
-	public function getInternetExplorerVersions() {
+	protected function getInternetExplorerVersions() {
 		return [
 			'1' => '1995-07-24',
 			'2' => '1995-11-27',
@@ -244,9 +320,9 @@ class generate {
 		];
 	}
 
-	protected function getOperaClassicVersions(?string $cache = null) : array|false {
+	protected function getOperaClassicVersions(bool $rebuild = false) : array|false {
 		$url = 'https://help.opera.com/en/operas-archived-history/';
-		if (($html = $this->fetch($url, $cache)) !== false) {
+		if (($html = $this->fetch($url)) !== false) {
 			$data = [];
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
@@ -256,18 +332,18 @@ class generate {
 						$data[\explode(' ', $cells->eq(0)->text())[1]] = $cells->eq(1)->text();
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
 				return $data;
 			}
 		}
 		return false;
 	}
 
-	public function getOperaVersions(?string $cache = null) : array|false {
+	protected function getOperaVersions(bool $rebuild = false) : array|false {
 		$url = 'https://en.wikipedia.org/wiki/History_of_the_Opera_web_browser';
-		if (($data = $this->getOperaClassicVersions($cache)) === false) {
+		$data = [];
+		if ($rebuild && ($data = $this->getOperaClassicVersions()) === false) {
 
-		} elseif (($html = $this->fetch($url, $cache)) !== false) {
+		} elseif (($html = $this->fetch($url)) !== false) {
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
 				foreach ($obj->find('p') AS $row) {
@@ -280,16 +356,15 @@ class generate {
 						$data[$match[2]] = $date->format('Y-m-d');
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
 			}
 		}
 		return $data ?: false;
 	}
 
-	public function getBraveVersions(?string $cache = null) : array|false {
-		$data = [];
+	protected function getBraveVersions(bool $rebuild = false) : array|false {
+		$data = $rebuild ? $this->getApkMirror('/uploads/?appcategory=brave-browser', 'Brave Private Web Browser, VPN ') : [];
 		$url = 'https://versions.brave.com/latest/brave-versions.json';
-		if (($file = $this->fetch($url, $cache)) !== false && ($json = \json_decode($file)) !== null) {
+		if (($file = $this->fetch($url)) !== false && ($json = \json_decode($file)) !== null) {
 			foreach ($json AS $item) {
 				if ($item->channel === 'release') {
 					$data[$item->name] = \substr($item->published, 0, 10);
@@ -299,10 +374,10 @@ class generate {
 		return $data ?: false;
 	}
 
-	public function getVivaldiVersions(?string $cache = null) : array|false {
+	protected function getVivaldiVersions(bool $rebuild = false) : array|false {
 		$data = [];
 		$url = 'https://vivaldi.com/download/archive/?platform=win';
-		if (($html = $this->fetch($url, $cache)) !== false) {
+		if (($html = $this->fetch($url)) !== false) {
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
 				foreach ($obj->find('tbody > tr') AS $row) {
@@ -311,17 +386,16 @@ class generate {
 						$data[\substr($text, 8, -8)] = $row->find('td:first-child + td')->text();
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
 				return $data;
 			}
 		}
 		return $data ?: false;
 	}
 
-	public function getMaxthonVersions(?string $cache = null) : array|false {
+	protected function getMaxthonVersions(bool $rebuild = false) : array|false {
 		$data = [];
 		$url = 'https://www.maxthon.com/history';
-		if (($html = $this->fetch($url, $cache)) !== false) {
+		if (($html = $this->fetch($url)) !== false) {
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
 				foreach ($obj->find('.history-list-text') AS $row) {
@@ -332,28 +406,30 @@ class generate {
 
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
 				return $data;
 			}
 		}
 		return $data ?: false;
 	}
 
-	protected function getApkMirror(string $path, string $prefix, ?string $cache = null) : array|false {
+	protected function getApkMirror(string $path, string $prefix, bool $rebuild = false, array $not = []) : array|false {
 		$data = [];
 		$len = \strlen($prefix);
 		while ($path !== null) {
-			if (($html = $this->fetch('https://www.apkmirror.com'.$path, $cache)) !== false) {
+			if (($html = $this->fetch('https://www.apkmirror.com'.$path)) !== false) {
 				$obj = new \hexydec\html\htmldoc();
 				if ($obj->load($html)) {
 					foreach ($obj->find('.table-row') AS $row) {
 						$title = $row->find('a.fontBlack')->text();
 						if (\str_starts_with($title, $prefix)) {
 							$date = new \DateTime($row->find('.visible-xs .dateyear_utc')->eq(0)->text());
-							$data[\explode(' ', \substr($title, $len))[0]] = $date->format('Y-m-d');
+							$version = \explode(' ', \substr($title, $len))[0];
+							if (!\in_array($version, $not)) {
+								$data[$version] = $date->format('Y-m-d');
+							}
 						}
 					}
-					$path = $obj->find('a[rel=next]')->attr('href');
+					$path = $rebuild ? $obj->find('a[rel=next]')->attr('href') : null;
 				} else {
 					break;
 				}
@@ -361,39 +437,100 @@ class generate {
 				break;
 			}
 		}
-		\ksort($data, SORT_NUMERIC);
 		return $data ?: false;
 	}
 
-	public function getSamsungInternetVersions(?string $cache = null) : array|false {
-		return $this->getApkMirror('/uploads/?appcategory=samsung-internet-for-android', 'Samsung Internet Browser ', $cache);
+	protected function getSamsungInternetVersions(bool $rebuild = false) : array|false {
+		return $this->getApkMirror('/uploads/?appcategory=samsung-internet-for-android', 'Samsung Internet Browser ', $rebuild);
 	}
 
-	public function getHuaweiBrowserVersions(?string $cache = null) : array|false {
-		return $this->getApkMirror('/uploads/?appcategory=huawei-browser', 'HUAWEI Browser ', $cache);
+	protected function getHuaweiBrowserVersions(bool $rebuild = false) : array|false {
+		return $this->getApkMirror('/uploads/?appcategory=huawei-browser', 'HUAWEI Browser ', $rebuild, ['50.50.50.6']);
 	}
 
-	public function getKmeleonVersions(?string $cache = null) : array|false {
+	protected function getUcBrowserVersions(bool $rebuild = false) : array|false {
+		return $this->getApkMirror('/uploads/?appcategory=uc-browser', 'UC Browser-Safe, Fast, Private ', $rebuild);
+	}
+
+	protected function getSilkBrowserVersions(bool $rebuild = false) : array|false {
+		return $this->getApkMirror('/uploads/?appcategory=silk-browser', 'Silk Browser ', $rebuild);
+	}
+
+	protected function getKmeleonVersions(bool $rebuild = false) : array|false {
 		$data = [];
 		$url = 'http://kmeleonbrowser.org/wiki/DownloadsArchive';
-		if (($html = $this->fetch($url, $cache)) !== false) {
+		if (($html = $this->fetch($url)) !== false) {
 			$obj = new \hexydec\html\htmldoc();
 			if ($obj->load($html)) {
 				foreach ($obj->find('.text-body h4') AS $row) {
 					$title = $row->text();
-					if (\preg_match('/K-Meleon ([0-9.]++)[^(]++\(([0-9-]++)\)/', $title, $match)) {
+					if (\preg_match('/([0-9.]++)[^(]++\(([0-9-]++)\)/', $title, $match)) {
 						$data[$match[1]] = $match[2];
 
 					}
 				}
-				\ksort($data, SORT_NUMERIC);
-				return $data;
 			}
 		}
 		return $data ?: false;
 	}
 
-	public function renderPhp(array $data) {
+	protected function getKonquerorVersions(bool $rebuild = false) : array|false {
+		$path = '/network/konqueror/-/tags';
+		$data = [];
+		while ($path !== null) {
+			if (($html = $this->fetch('https://invent.kde.org'.$path)) !== false) {
+				$obj = new \hexydec\html\htmldoc();
+				if ($obj->load($html)) {
+					foreach ($obj->find('.content-list > li') AS $row) {
+						$title = \ltrim($row->find('a.gl-font-bold')->text(), 'v');
+						$date = new \DateTime($row->find('time')->attr('datetime'));
+						$data[$title] = $date->format('Y-m-d');
+					}
+					$path = $rebuild ? $obj->find('a[rel=next]')->attr('href') : null;
+				} else {
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+		return $data ?: false;
+	}
+
+	protected function getWaterfoxVersions(bool $rebuild = false) : array|false {
+		$data = [];
+		$url = 'https://www.waterfox.net/download/';
+		if (($html = $this->fetch($url)) === false) {
+
+		} elseif (($start = \mb_strpos($html, '(function(){const items = [{')) === false) {
+			
+		} elseif (($end = \mb_strpos($html, ';', $start)) === false) {
+
+		} elseif (($json = \mb_substr($html, $start + 26, $end - $start - 26)) === false) {
+			
+		} elseif (($items = \json_decode($json, true)) === null) {
+			
+		} else {
+			foreach ($items AS $item) {
+				$data[$item['label']] = (new \DateTime($item['pubDate']))->format('Y-m-d');
+			}
+		}
+		return $data ?: false;
+	}
+
+	protected function getPalemoonVersions(bool $rebuild = false) : array|false {
+		$data = [];
+		$url = 'https://repo.palemoon.org/MoonchildProductions/Pale-Moon/releases.rss';
+		if (($xml = $this->fetch($url)) !== false) {
+			$obj = \simplexml_load_string($xml);
+			foreach ($obj->xpath('//channel/item') AS $item) {
+				$data[\mb_substr((string) $item->title, 10)] = (new \DateTime((string) $item->pubDate))->format('Y-m-d');
+			}
+		}
+		return $data ?: false;
+	}
+
+	protected function renderPhp(array $data) {
 		$php = [
 			'<?php',
 			'declare(strict_types=1);',
